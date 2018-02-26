e16fc91455b369ffed13b33a46d5d9081ecec98a
LMQ <LMQ>
2018-02-26 11:45:33 +0800

    增加 一个统计例子

--- src/main/scala/com/flink/common/param/EnvironmentalKey.scala
+++ src/main/scala/com/flink/common/param/EnvironmentalKey.scala
@@ -1,8 +1,7 @@
 package com.flink.common.param
 
 trait EnvironmentalKey {
-  val KAFKA_BROKER = "kylin-node4:9092,kylin-node3:9092,kylin-node2:9092"
-  val HBASE_ZOOKEEPER = "kylin-node4,kylin-node3,kylin-node2"
+  val HBASE_ZOOKEEPER = "solr2.zhiziyun.com,solr1.zhiziyun.com,mongodb3"
   val KAFKA_ZOOKEEPER = "solr2.zhiziyun.com,solr1.zhiziyun.com,mongodb3"
   val BROKER = "kafka1:9092,kafka2:9092,kafka3:9092"
   val TOPIC = "smartadsdeliverylog,smartadsclicklog"
