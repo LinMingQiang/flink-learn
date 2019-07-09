af15db5ee9d41082271f81d3ad2aa75c3a1cea8e
LinMingQiang <1356469429@qq.com>
2019-07-09 08:13:17 +0800

    过期的schema

--- src/main/scala/com/flink/common/entry/TopicMessageDeserialize.scala
+++ src/main/scala/com/flink/common/entry/TopicMessageDeserialize.scala
@@ -1,12 +1,13 @@
 package com.flink.common.entry
 
-import org.apache.flink.streaming.util.serialization.KeyedDeserializationSchema
 import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.api.common.typeinfo.BasicTypeInfo
+import org.apache.flink.streaming.connectors.kafka.KafkaDeserializationSchema
+import org.apache.kafka.clients.consumer.ConsumerRecord
 
-class TopicMessageDeserialize extends KeyedDeserializationSchema[(String,String)]{
- override def deserialize(x:Array[Byte],x2:Array[Byte], topic:String ,partition:Int , offset:Long )={
-   (topic,new String(x2))
+class TopicMessageDeserialize extends KafkaDeserializationSchema[(String,String)]{
+ override def deserialize(record:ConsumerRecord[Array[Byte],Array[Byte]])={
+   (record.topic,new String(record.value()))
  }
  override def isEndOfStream(nextElement:(String,String))={
    false
