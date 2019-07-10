c0abe37d0c61acdb4e74d0b3336ae865ced04806
LinMingQiang <1356469429@qq.com>
2019-07-10 14:25:32 +0800

    getFlinkEnv

--- src/main/scala/com/flink/common/entry/package.scala
+++ src/main/scala/com/flink/common/entry/package.scala
@@ -3,13 +3,15 @@ package com.flink.common
 import com.flink.common.param.EnvironmentalKey
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.contrib.streaming.state.RocksDBStateBackend
+import org.apache.flink.streaming.api.environment.CheckpointConfig.ExternalizedCheckpointCleanup
+
 package object entry extends EnvironmentalKey {
 
   def getFlinkEnv() = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
-    env.setParallelism(3)
+    env.setParallelism(1)
     env.enableCheckpointing(6000) //更新offsets。每60s提交一次
-    
+    env.getCheckpointConfig.enableExternalizedCheckpoints(ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION)
     env.setStateBackend((new RocksDBStateBackend("file:///C:\\Users\\Administrator\\Desktop\\checkpoint")))
     env
   }
