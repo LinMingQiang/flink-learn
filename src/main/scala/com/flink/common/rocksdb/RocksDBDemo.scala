a715f24c98f29b57b0ebef6304430ba29bf13c70
LinMingQiang <1356469429@qq.com>
2019-07-11 18:32:42 +0800

    rocksdbDemo

---
+++ src/main/scala/com/flink/common/rocksdb/RocksDBDemo.scala
@@ -0,0 +1,23 @@
+package com.flink.common.rocksdb
+
+import org.rocksdb.RocksDB
+import org.rocksdb.Options
+import org.rocksdb.util.SizeUnit
+import org.rocksdb.CompactionStyle
+object RocksDBDemo {
+  val filep = "C:\\Users\\Administrator\\Desktop\\test.db"
+  def main(args: Array[String]): Unit = {
+    RocksDB.loadLibrary()
+    val options = new Options()
+      .setCreateIfMissing(true)
+      .setWriteBufferSize(256 * SizeUnit.MB)
+      .setMaxWriteBufferNumber(4)
+      .setMaxBackgroundCompactions(16)
+      .setCompactionStyle(CompactionStyle.UNIVERSAL)
+    val db = RocksDB.open(options, filep)
+    val f = db.put("hello".getBytes, "world".getBytes)
+    println(new String(db.get("hello".getBytes)))
+    //Thread.sleep(10000)
+    db.close()
+  }
+}
