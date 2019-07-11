fddf1efc9a53390c5c4202d96505c5f73d98a27c
LinMingQiang <1356469429@qq.com>
2019-07-11 23:45:07 +0800

    update

--- src/main/scala/com/flink/common/entry/package.scala
+++ src/main/scala/com/flink/common/entry/package.scala
@@ -7,15 +7,20 @@ import org.apache.flink.runtime.state.filesystem.FsStateBackend
 import org.apache.flink.streaming.api.environment.CheckpointConfig.ExternalizedCheckpointCleanup
 
 package object entry extends EnvironmentalKey {
-  def getFlinkEnv() = {
+
+  def getFlinkEnv(checkpointPath :String) = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
-    env.setParallelism(6)
+
     env.enableCheckpointing(6000) //更新offsets。每60s提交一次
-    env.getCheckpointConfig.setMaxConcurrentCheckpoints(5)
     env.getCheckpointConfig.enableExternalizedCheckpoints(ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION)
-    //env.setStateBackend(new FsStateBackend(("hdfs:///solr2/user/linmingqiang/checkpoint/")))
+    //超时
+    //env.getCheckpointConfig.setCheckpointTimeout(5000)
+    // 同一时间只允许进行一个检查点
+    env.getCheckpointConfig.setMaxConcurrentCheckpoints(1);
+    // 表示一旦Flink处理程序被cancel后，会保留Checkpoint数据，以便根据实际需要恢复到指定的Checkpoint
+    env.getCheckpointConfig.enableExternalizedCheckpoints(ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
     //env.setStateBackend(new FsStateBackend(("file:///C:\\Users\\Master\\Desktop\\fscheckpoint")))
-    env.setStateBackend((new RocksDBStateBackend("file:///C:\\Users\\Administrator\\Desktop\\rocksdbcheckpoint")))
+    env.setStateBackend((new RocksDBStateBackend(checkpointPath)))
     env
   }
 }
