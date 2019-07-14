ca7c3e4b232d889907ffcfd4082e540fc1496e2c
LinMingQiang <1356469429@qq.com>
2019-07-14 14:30:56 +0800

    operate state

--- src/main/scala/com/flink/common/entry/package.scala
+++ src/main/scala/com/flink/common/entry/package.scala
@@ -15,10 +15,10 @@ package object entry extends EnvironmentalKey {
     * @param checkpointPath
     * @return
     */
-  def getFlinkEnv(checkpointPath: String) = {
+  def getFlinkEnv(checkpointPath: String,interval:Long = 6000) = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
 
-    env.enableCheckpointing(6000) //更新offsets。每60s提交一次
+    env.enableCheckpointing(interval) //更新offsets。每60s提交一次
     //超时
     //env.getCheckpointConfig.setCheckpointTimeout(5000)
     // 同一时间只允许进行一个检查点
@@ -36,9 +36,9 @@ package object entry extends EnvironmentalKey {
   /**
     *
     */
-  def getKafkaParam() = {
+  def getKafkaParam(broker: String) = {
     val pro = new Properties()
-    pro.put("bootstrap.servers", BROKER)
+    pro.put("bootstrap.servers", broker)
     pro.put("zookeeper.connect", KAFKA_ZOOKEEPER)
     pro.put("group.id", "test")
     pro.put("auto.commit.enable", "true") //kafka 0.8-
