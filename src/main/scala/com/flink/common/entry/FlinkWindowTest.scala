6a93c9af1c1705d3d2a87b322317b9e66734a4ca
linmingqiang <1356469429@qq.com>
2019-10-12 16:30:25 +0800

    flink : operator state 管理

--- src/main/scala/com/flink/common/entry/FlinkWindowTest.scala
+++ src/main/scala/com/flink/common/entry/FlinkWindowTest.scala
@@ -2,32 +2,23 @@ package com.flink.common.entry
 
 import com.alibaba.fastjson.JSON
 import com.flink.common.deserialize.TopicMessageDeserialize
-import org.apache.flink.streaming.api.windowing.assigners.{ProcessingTimeSessionWindows, TumblingProcessingTimeWindows}
-import org.apache.flink.streaming.api.windowing.time.Time
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010
 import org.apache.flink.streaming.api.scala._
-
-import scala.collection.JavaConversions._
 object FlinkWindowTest {
   val checkpointPath =
-    "file:///C:\\Users\\mqlin\\Desktop\\testdata\\flink\\rocksdbcheckpoint"
+    "file:///C:\\Users\\mqlin\\Desktop\\testdata\\flink\\checkpoint\\FlinkWindowTest"
   def main(args: Array[String]): Unit = {
     val env = getFlinkEnv(checkpointPath, 3000) // 1 min
-    val kafkasource = new FlinkKafkaConsumer010[KafkaMessge](
-      TOPIC.split(",").toList,
-      new TopicMessageDeserialize(),
-      getKafkaParam(BROKER))
+    val kafkasource = getKafkaSource(TOPIC, BROKER,  new TopicMessageDeserialize())
     kafkasource.setCommitOffsetsOnCheckpoints(true)
     kafkasource.setStartFromLatest() //不加这个默认是从上次消费
     val result = env
       .addSource(kafkasource)
-      .map(x => ("c", 1))
-      // .filter(_._1 == "2412")
-      .keyBy(0)
+       .map(x => (JSON.parseObject(x.msg).getString("dist"), 1))
+       .keyBy(0)
       // .window(ProcessingTimeSessionWindows.withGap(Time.seconds(10)))
       // .window(TumblingProcessingTimeWindows.of(Time.seconds(10))) // = .timeWindow(Time.seconds(60))
       //
-      .sum(1)
+       .sum(1)
       .print
     env.execute("jobname")
   }
