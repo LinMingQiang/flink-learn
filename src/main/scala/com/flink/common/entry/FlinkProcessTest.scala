2b1d938432c537330e719d85f1a67bc8e9b60667
linmingqiang <1356469429@qq.com>
2019-10-13 15:44:06 +0800

    process func

---
+++ src/main/scala/com/flink/common/entry/FlinkProcessTest.scala
@@ -0,0 +1,29 @@
+package com.flink.common.entry
+
+import com.alibaba.fastjson.JSON
+import com.flink.common.deserialize.TopicOffsetMsgDeserialize
+import com.flink.common.richf.CountWithTimeoutProcessFunction
+import com.flink.common.sink.OperatorStateBufferingSink
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.scala._
+
+import scala.collection.JavaConversions._
+
+object FlinkProcessTest {
+  // 可以实现类似于 session 的功能， 某个key在某个session里面的访问次数。例如 session的间隔为 60s
+  val checkpointPath = "file:///C:\\Users\\mqlin\\Desktop\\testdata\\flink\\checkpoint\\FlinkProcess"
+  def main(args: Array[String]): Unit = {
+    val env = getFlinkEnv(checkpointPath, 60000) // 1 min
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+    val kafkasource = getKafkaSource(TOPIC, BROKER,  new TopicOffsetMsgDeserialize())
+    kafkasource.setCommitOffsetsOnCheckpoints(true)
+    kafkasource.setStartFromLatest() //不加这个默认是从上次消费
+   env
+      .addSource(kafkasource)
+      .map(x => (JSON.parseObject(x.msg).getString("dist"),""))
+      .keyBy(0)
+      .process(new CountWithTimeoutProcessFunction)
+      .print
+    env.execute("FlinkOperatorStateTest")
+  }
+}
