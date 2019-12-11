8d6555c4389634de9c6b5000cb2b6472aeeb80b5
LinMingQiang <1356469429@qq.com>
2019-12-11 14:24:09 +0800

    Update EnvironmentalKey.scala

--- flink-demo/src/main/scala/com/flink/common/param/EnvironmentalKey.scala
+++ flink-demo/src/main/scala/com/flink/common/param/EnvironmentalKey.scala
@@ -3,6 +3,6 @@ package com.flink.common.param
 trait EnvironmentalKey {
   val HBASE_ZOOKEEPER = ""
   val KAFKA_ZOOKEEPER = "ucloud-cdh-04,ucloud-cdh-05,ucloud-cdh-06"
-  val BROKER = "10.6.161.208:9092,10.6.161.209:9092,10.6.161.210:9092,10.6.161.211:9092,10.6.161.212:9092"
-  val TOPIC = "mobssprequestlog_p15"
+  val BROKER = "local"
+  val TOPIC = "topic"
 }
