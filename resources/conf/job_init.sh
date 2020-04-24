91ba9bd03c778c8c312af2e59314fc0ecc22c06c
mqlin <1356469429@qq.com>
2020-04-24 17:57:04 +0800

    项目配置化

---
+++ resources/conf/job_init.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# 初始化环境
+set -x
+export PRO_ENV=${flinklearn.env}
+export PRO_HOME="$(cd "`dirname "$0"`"/..; pwd)"
+profilePath=$PRO_HOME/conf/application.properties
+ddl_sql_jar=$PRO_HOME/lib/flink-sql-${project.version}.jar
+log_path=$PRO_HOME/conf/log4j.properties
+
