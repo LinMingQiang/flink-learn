49543907faf0ae595c4c650fe28376c1783a129d
mqlin <1356469429@qq.com>
2020-04-27 11:58:07 +0800

    功能分离

---
+++ flink-sql/src/main/scala/com/flink/learn/sql/common/SchemaManager.scala
@@ -0,0 +1,12 @@
+package com.flink.learn.sql.common
+
+import org.apache.flink.api.common.typeinfo.Types
+import org.apache.flink.table.descriptors.Schema
+
+object SchemaManager {
+  //默认加一个PROCESSING_TIME时间属性字段
+  val ID_NAME_AGE_SCHEMA = new Schema()
+    .field("id", Types.STRING)
+    .field("name", Types.STRING)
+    .field("age", Types.INT)
+}
---
+++ flink-sql/src/main/scala/com/flink/learn/sql/common/SqlConnectorManager.scala
@@ -0,0 +1,33 @@
+package com.flink.learn.sql.common
+
+import org.apache.flink.table.descriptors.Kafka
+
+object SqlConnectorManager {
+
+  /**
+    *
+    * @param broker
+    * @param topic
+    * @param groupid
+    * @param from
+    * @return
+    */
+  def kafkaConnector(broker: String,
+                     topic: String,
+                     groupid: String,
+                     from: String): Kafka = {
+    val kafkaConnector = new Kafka()
+      .version("0.10")
+      .topic(topic)
+      .property("bootstrap.servers", broker)
+      .property("group.id", groupid)
+      .property("zookeeper.connect", "localhost:2181")
+    from match {
+      case "consum"   => kafkaConnector.startFromGroupOffsets()
+      case "latest"   => kafkaConnector.startFromLatest()
+      case "earliest" => kafkaConnector.startFromEarliest()
+      case _          => kafkaConnector.startFromLatest()
+    }
+  }
+
+}
