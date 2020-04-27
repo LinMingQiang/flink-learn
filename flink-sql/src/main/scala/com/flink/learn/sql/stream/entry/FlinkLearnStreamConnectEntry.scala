bdf0df001b644208807d93ae98287c344714670d
mqlin <1356469429@qq.com>
2020-04-27 16:15:16 +0800

    代码整理

--- flink-sql/src/main/scala/com/flink/learn/sql/stream/entry/FlinkLearnStreamConnectEntry.scala
+++ flink-sql/src/main/scala/com/flink/learn/sql/stream/entry/FlinkLearnStreamConnectEntry.scala
@@ -1,18 +1,21 @@
 package com.flink.learn.sql.stream.entry
 
+import com.flink.common.core.FlinkLearnPropertiesUtil.BROKER
 import com.flink.common.core.{FlinkEvnBuilder, FlinkLearnPropertiesUtil}
 import com.flink.learn.sql.common.{
   DataFormatUril,
   SchemaManager,
   SqlConnectorManager
 }
+import org.apache.flink.api.common.time.Time
 import org.apache.flink.api.common.typeinfo.Types
-import org.apache.flink.table.api.EnvironmentSettings
+import org.apache.flink.table.api.{EnvironmentSettings, StreamQueryConfig}
 import org.apache.flink.table.api.scala.StreamTableEnvironment
 import org.apache.flink.table.descriptors.{Csv, Json, Kafka, Schema}
 import org.apache.flink.types.Row
 import org.apache.flink.api.scala._
 import org.apache.flink.table.api.scala._
+import FlinkLearnPropertiesUtil._
 
 object FlinkLearnStreamConnectEntry {
 
@@ -25,27 +28,82 @@ object FlinkLearnStreamConnectEntry {
       "/Users/eminem/workspace/flink/flink-learn/dist/conf/application.properties",
       "test")
     // val env = StreamExecutionEnvironment.getExecutionEnvironment
-    import FlinkLearnPropertiesUtil._
     val streamTableEnv = FlinkEvnBuilder.buildStreamTableEnv(param,
                                                              CHECKPOINT_PATH,
                                                              10000) // 1 min
+    createTestTbl(streamTableEnv)
+    createTest2Tbl(streamTableEnv)
+
+
+
+    streamTableEnv.execute("FlinkLearnStreamConnectEntry")
+  }
+  // {"id":"1","name":"211","age":5}
+
+  /**
+    *
+    * @param streamTableEnv
+    */
+  def createTestTbl(streamTableEnv: StreamTableEnvironment): Unit = {
     // kafka source
     val kafkaConnector =
       SqlConnectorManager.kafkaConnector(BROKER, "test", "test", "latest")
+    val jsonFormat = DataFormatUril.kafkaConnJsonFormat(kafkaConnector)
+    // lazy val csvFormat = DataFormatUril.kafkaConnCsvFormat(kafkaConnector)
     streamTableEnv
       .connect(kafkaConnector)
-      .withFormat(DataFormatUril.kafkaConnJsonFormat(kafkaConnector))
+      .withFormat(jsonFormat)
       .withSchema(SchemaManager.ID_NAME_AGE_SCHEMA)
       .inAppendMode()
-      .registerTableSource("test")
-   // excute sql
+      .createTemporaryTable("test")
+  }
+
+  /**
+    *
+    * @param streamTableEnv
+    */
+  def createTest2Tbl(streamTableEnv: StreamTableEnvironment): Unit = {
+    // kafka source
+    val kafkaConnector =
+      SqlConnectorManager.kafkaConnector(BROKER, "test2", "test", "latest")
+    val jsonFormat = DataFormatUril.kafkaConnJsonFormat(kafkaConnector)
+    // lazy val csvFormat = DataFormatUril.kafkaConnCsvFormat(kafkaConnector)
     streamTableEnv
-      .sqlQuery(s"""select id,count(distinct name) from test group by id""")
-      .toRetractStream[Row]
+      .connect(kafkaConnector)
+      .withFormat(jsonFormat)
+      .withSchema(SchemaManager.ID_NAME_AGE_SCHEMA)
+      .inAppendMode()
+      .createTemporaryTable("test2")
+  }
+
+  /**
+    *
+    * @param streamTableEnv
+    */
+  def streamJoin(streamTableEnv: StreamTableEnvironment): Unit = {
+    streamTableEnv
+      .sqlQuery(s"""select a.*,b.* from test a join test2 b on a.id = b.id""")
+      .toRetractStream[Row](
+        new StreamQueryConfig(Time.minutes(1).toMilliseconds,
+                              Time.minutes(6).toMilliseconds))
       .filter(_._1)
       .map(_._2)
       .print()
-    streamTableEnv.execute("FlinkLearnStreamConnectEntry")
   }
-  // {"id":"2","name":"6","age":5}
+
+  /**
+    *
+    * @param streamTableEnv
+    */
+  def streamCountDistinct(streamTableEnv: StreamTableEnvironment): Unit = {
+    // excute sql
+    streamTableEnv
+      .sqlQuery(s"""select a.*,b.* from test a join test2 b on a.id = b.id""")
+      .toRetractStream[Row](
+        new StreamQueryConfig(Time.minutes(1).toMilliseconds,
+                              Time.minutes(6).toMilliseconds))
+      .filter(_._1)
+      .map(_._2)
+      .print()
+  }
 }
