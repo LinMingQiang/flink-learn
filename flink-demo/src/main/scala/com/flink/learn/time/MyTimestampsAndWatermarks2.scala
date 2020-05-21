e0794c3db206a8acca6f3aa3bbaceb6456638bf8
mqlin <1356469429@qq.com>
2020-05-21 16:52:43 +0800

    session window： 关于session窗口的注释

--- flink-demo/src/main/scala/com/flink/learn/time/MyTimestampsAndWatermarks2.scala
+++ flink-demo/src/main/scala/com/flink/learn/time/MyTimestampsAndWatermarks2.scala
@@ -4,20 +4,29 @@ import com.flink.learn.bean.CaseClassUtil.{SessionLogInfo, Wordcount}
 import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks
 import org.apache.flink.streaming.api.watermark.Watermark
 
+/**
+  *
+  * @param maxOutOfOrderness 最长延迟时间
+  */
 class MyTimestampsAndWatermarks2(maxOutOfOrderness: Long)
     extends AssignerWithPeriodicWatermarks[SessionLogInfo] {
-  var currentMaxTimestamp = 0L
+  var currentMaxTimestamp = 0L // 当前数据最大的时间戳
 
   /**
-    * 当前最大的时间戳 - 最多延迟多久
-    * @return
+    * @return 获取wartermark。用当前最大的数据时间戳- 最多延迟多久
     */
   override def getCurrentWatermark: Watermark = {
-    val w = new Watermark(currentMaxTimestamp - maxOutOfOrderness)
-    w
+    new Watermark(currentMaxTimestamp - maxOutOfOrderness)
   }
 
+  /**
+    * 提取数据里面的时间戳
+    * @param element
+    * @param l
+    * @return
+    */
   override def extractTimestamp(element: SessionLogInfo, l: Long): Long = {
+    // 更新当前的最大时间戳
     currentMaxTimestamp = Math.max(element.timeStamp, currentMaxTimestamp)
     element.timeStamp
   }
