cc5c6925a50047f2f3748db84bc43be54adc9dea
mqlin <1356469429@qq.com>
2020-07-01 16:54:55 +0800

    java state processor

--- flink-demo/src/main/java/com/flink/learn/trans/AccountJavaKeyedStateBootstrapFunction.java
+++ flink-demo/src/main/java/com/flink/learn/trans/AccountJavaKeyedStateBootstrapFunction.java
@@ -9,18 +9,23 @@ import org.apache.flink.configuration.Configuration;
 import org.apache.flink.state.api.functions.KeyedStateBootstrapFunction;
 
 public class AccountJavaKeyedStateBootstrapFunction extends KeyedStateBootstrapFunction<String, TranWordCountPoJo> {
-    ValueState<TranWordCountPoJo> lastState;
+   // ValueState<TranWordCountPoJo> lastState;
+   ValueState<WordCountPoJo> lastState;
 
     @Override
     public void open(Configuration parameters) {
-        ValueStateDescriptor<TranWordCountPoJo> descriptor =
-                new ValueStateDescriptor<TranWordCountPoJo>("wordcountState", TranWordCountPoJo.class);
+        ValueStateDescriptor<WordCountPoJo> descriptor =
+                new ValueStateDescriptor("wordcountState", WordCountPoJo.class);
         lastState = getRuntimeContext().getState(descriptor);
     }
 
     @Override
     public void processElement(TranWordCountPoJo value, Context ctx) throws Exception {
         value.count = 10L;
-        lastState.update(value);
+        WordCountPoJo w = new WordCountPoJo();
+        w.word = value.word;
+        w.count = 1000L;
+        w.timestamp = value.timestamp;
+        lastState.update(w);
     }
 }
