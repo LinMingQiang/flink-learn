58ff911f52d72da41907d419c35873b8f71cd7e9
mqlin <1356469429@qq.com>
2020-07-08 18:32:09 +0800

    增加了 stream join 测试

--- flink-util/src/main/scala/com/flink/common/core/EnvironmentalKey.scala
+++ flink-util/src/main/scala/com/flink/common/core/EnvironmentalKey.scala
@@ -2,4 +2,7 @@ package com.flink.common.core
 
 object EnvironmentalKey {
   val LOCAL_PROPERTIES_PATH = "/Users/eminem/workspace/flink/flink-learn/dist/conf/application.properties"
+  val HBASE_ZOOKEEPER = "localhost:2181"
+  val BROKER = "localhost:9092"
+  val TOPIC = "test"
 }
