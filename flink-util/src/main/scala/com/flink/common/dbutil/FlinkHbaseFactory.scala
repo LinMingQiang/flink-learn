b12a4791cf69b3ada1b25f12af6a6b20c16c520b
mqlin <1356469429@qq.com>
2020-07-22 17:21:17 +0800

    自定义 hbase sink factory

--- flink-util/src/main/scala/com/flink/common/dbutil/FlinkHbaseFactory.scala
+++ flink-util/src/main/scala/com/flink/common/dbutil/FlinkHbaseFactory.scala
@@ -1,46 +1,63 @@
 package com.flink.common.dbutil
 
+import java.util
 import java.util.HashMap
 
 import com.flink.common.core.EnvironmentalKey
+//import com.stumbleupon.async.Callback
 import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
 import org.apache.hadoop.hbase.client._
+//import org.hbase.async.{GetRequest, HBaseClient, KeyValue}
 
 import scala.collection.JavaConversions._
 
 object FlinkHbaseFactory {
   var conn: Connection = null
-  var tables: HashMap[String, Table] = new HashMap[String, Table]
-  def initConn() {
+//  var asyncClient : HBaseClient = null
+  def initConn(zk: String) {
     if (conn == null || conn.isClosed()) {
       println("----  Init Conn  -----")
-      var hconf = HBaseConfiguration.create()
-      hconf.set("hbase.zookeeper.quorum", EnvironmentalKey.HBASE_ZOOKEEPER)
+      val hconf = HBaseConfiguration.create()
+      hconf.set("hbase.zookeeper.quorum", zk)
       hconf.set("hbase.zookeeper.property.clientPort", "2181")
       conn = ConnectionFactory.createConnection(hconf)
     }
   }
-  def getConn() = {
-    initConn
+
+  def getGlobalConn(zk: String) = {
+    initConn(zk)
     conn
   }
-  def getTable(tablename: String) = {
-    tables.getOrElse(tablename, {
-      initConn
-      conn.getTable(TableName.valueOf(tablename))
-    })
+
+//  def getGlobalAsyncConn(zk: String) ={
+//  if(asyncClient == null){
+    //      asyncClient = new HBaseClient(zk)
+    //    }
+    //    asyncClient
+//  }
+
+
+  def main(args: Array[String]): Unit = {
+//    getGlobalAsyncConn("localhost:2181")
+//    val r = asyncClient.get(new GetRequest("test".getBytes, "test".getBytes()))
+//    val rr = r.addCallback(new Callback[String, util.ArrayList[KeyValue]](){
+//      override def call(t: util.ArrayList[KeyValue]): String = {
+//        print(",,,,,")
+//        t.map(x => {
+//          println(x)
+//          new String(x.value())
+//        }).mkString("|")
+//      }
+//    })
+//    asyncClient.shutdown()
+
+    val  conn = getGlobalConn("localhost:2181")
+    conn.getAdmin.listTableNames().foreach(println)
+    val test =   conn.getTable(TableName.valueOf("test"))
+    println(test.exists(new Get("test".getBytes())))
+    println(test.get(new Get("test".getBytes())).getValue("info".getBytes(),"v".getBytes()))
+    conn.close()
+   // println(">>>>>>>" + r)
   }
-  def put(tableName: String, p: Put) {
-    getTable(tableName)
-      .put(p)
-  }
-  def get(tableName: String, get: Get, columns: Array[String]) = {
-    val r = getTable(tableName)
-      .get(get)
-    if (r != null && !r.isEmpty()) {
-      columns.map { x =>
-        new String(r.getValue("info".getBytes, x.getBytes))
-      }
-    } else null
-  }
+
 }
