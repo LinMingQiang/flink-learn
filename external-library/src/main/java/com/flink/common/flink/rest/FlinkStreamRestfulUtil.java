931e94966e0b34c42d1edc9195a00d5ba651fca3
linmingqiang <lmq19911010>
2020-08-04 23:04:33 +0800

    bigdata restful api

---
+++ external-library/src/main/java/com/flink/common/flink/rest/FlinkStreamRestfulUtil.java
@@ -0,0 +1,12 @@
+package com.flink.common.flink.rest;
+
+public class FlinkStreamRestfulUtil {
+
+    public static void getJobid(){
+
+    }
+
+    public static void getJobCheckpoint(){
+
+    }
+}
