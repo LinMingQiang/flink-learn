23eba2d70253e9f900d2f3c77082c2e798faf31d
mqlin <1356469429@qq.com>
2020-08-04 18:05:52 +0800

    第三方相关：Monitor；通过yarn，flink的rest来获取运行状态信息

---
+++ external-library/src/test/java/TestMonitor.java
@@ -0,0 +1,7 @@
+import com.flink.common.yarn.api.YarnClientHandler;
+
+public class TestMonitor {
+    public static void main(String[] args) {
+        YarnClientHandler yarnclient = YarnClientHandler.getInstance(null);
+    }
+}
