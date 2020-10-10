7dfbec1a746803009e5adc9b146d31c3e91d8c08
mqlin <1356469429@qq.com>
2020-10-10 16:20:38 +0800

    sql自定义函数，返回多个字段

--- flink-sql/src/main/scala/com/flink/learn/sql/func/StrToLowOrUpScalarFunction.scala
+++ flink-sql/src/main/scala/com/flink/learn/sql/func/StrToLowOrUpScalarFunction.scala
@@ -1,14 +1,22 @@
 package com.flink.learn.sql.func
 
+import org.apache.commons.lang3.time.DateFormatUtils
+import org.apache.flink.table.annotation.DataTypeHint
 import org.apache.flink.table.functions.{FunctionContext, ScalarFunction}
+import org.apache.flink.types.Row
 
 object StrToLowOrUpScalarFunction extends ScalarFunction {
   var hashcode_factor: String = null
+
   override def open(context: FunctionContext): Unit = {
     // access "hashcode_factor" parameter
     // "12" would be the default value if parameter does not exist
     hashcode_factor = context.getJobParameter("loworupper", "UP")
   }
+
+  @DataTypeHint("ROW<s STRING>")
+  def eval(tsamp: Long, format: String): Row =
+    Row.of(DateFormatUtils.format(tsamp, format))
 
   /**
     *
