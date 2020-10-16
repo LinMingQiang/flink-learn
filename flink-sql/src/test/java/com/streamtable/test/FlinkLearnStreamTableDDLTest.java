819c1111f238106642f26fd37fc2951f4c623719
mqlin <1356469429@qq.com>
2020-10-16 11:09:05 +0800

    stream connect stream

--- flink-sql/src/test/java/com/streamtable/test/FlinkLearnStreamTableDDLTest.java
+++ flink-sql/src/test/java/com/streamtable/test/FlinkLearnStreamTableDDLTest.java
@@ -2,6 +2,9 @@ package com.streamtable.test;
 
 import com.flink.learn.sql.common.DDLSourceSQLManager;
 import com.flink.learn.test.common.FlinkJavaStreamTableTestBase;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.functions.co.CoMapFunction;
 import org.apache.flink.types.Row;
 import org.junit.Test;
 
@@ -26,7 +29,56 @@ public class FlinkLearnStreamTableDDLTest extends FlinkJavaStreamTableTestBase {
 
 
     @Test
-    public void testDDLWindow(){
+    public void testDDLWindow() {
 
     }
+
+    @Test
+    public void testStreamConneect() throws Exception {
+
+        tableEnv.executeSql(
+                DDLSourceSQLManager.createStreamFromKafka_CSV("localhost:9092",
+                        "localhost:2181",
+                        "test",
+                        "test",
+                        ",",
+                        "test"));
+        tableEnv.executeSql(
+                DDLSourceSQLManager.createStreamFromKafka_CSV("localhost:9092",
+                        "localhost:2181",
+                        "test2",
+                        "test2",
+                        ",",
+                        "test2"));
+
+        tableEnv.executeSql(DDLSourceSQLManager.createCustomPrintlnRetractSinkTbl("printlnSinkTbl"));
+
+        DataStream<Row> a1 =
+                tableEnv.toAppendStream(
+                        tableEnv.sqlQuery("select topic,msg from test") , Row.class);
+        DataStream<Row> a2 =
+                tableEnv.toAppendStream(
+                        tableEnv.sqlQuery("select topic,msg from test2") , Row.class);
+
+        DataStream a3 = a1.connect(a2).map(new CoMapFunction<Row, Row, TestPoJo>() {
+            @Override
+            public TestPoJo map1(Row x) throws Exception {
+                TestPoJo r = new TestPoJo(x.getField(0).toString(), x.getField(1).toString(), 1L);
+                        return r;
+            }
+            @Override
+            public TestPoJo map2(Row x) throws Exception {
+                TestPoJo r = new TestPoJo(x.getField(0).toString(), x.getField(1).toString(), 1L);
+                return r;
+            }
+        }).returns(Types.POJO(TestPoJo.class));
+
+        tableEnv.createTemporaryView("tmptale1", tableEnv.fromDataStream(a3));
+        tableEnv.sqlQuery("select topic,msg,sum(ll) ll from tmptale1 group by topic,msg")
+                .insertInto("printlnSinkTbl");
+
+        tableEnv.execute("");
+    }
+
+
 }
