0eed1e99058a8d133222576454b8327f6c1960a8
mqlin <1356469429@qq.com>
2021-02-25 18:10:38 +0800

    flink -> hive

--- flink-sql/src/test/java/com/streamhive/test/StreamDataWriteToHiveTest.java
+++ flink-sql/src/test/java/com/streamhive/test/StreamDataWriteToHiveTest.java
@@ -10,36 +10,43 @@ import org.junit.Test;
 public class StreamDataWriteToHiveTest extends FlinkJavaStreamTableTestBase {
     @Test
     public void testHiveCatalog() {
-        // {"rowtime":"2021-01-20 00:00:23","msg":"hello"}
+        // {"rowtime":"2021-01-20 20:01:23","msg":"hello"}
         HiveCatalog catalog = new HiveCatalog(
                 "catalogName",              // catalog name
                 "default",                // default database
                 "/Users/eminem/workspace/flink/flink-learn/resources/conf",  // Hive config (hive-site.xml) directory
                 "2.3.2"                   // Hive version
         );
-//        String sql = DDLSourceSQLManager.createStreamFromKafka("localhost:9092",
-//                "test",
-//                "test",
-//                "test",
-//                "json");
-//        System.out.println(sql);
-//        tableEnv.executeSql(
-//                sql);
+        String sql = DDLSourceSQLManager.createStreamFromKafka("localhost:9092",
+                "test",
+                "test",
+                "test",
+                "json");
+        System.out.println(sql);
+        tableEnv.executeSql(
+                sql);
 
 
+        tableEnv.registerCatalog("catalogName", catalog);
+        tableEnv.useCatalog("catalogName");
+        tableEnv.getConfig().setSqlDialect(SqlDialect.HIVE);
+        TableResult r = tableEnv.executeSql("INSERT INTO TABLE hive_table SELECT msg as user_id, 1.1 as order_amount, DATE_FORMAT(rowtime, 'yyyy-MM-dd'), DATE_FORMAT(rowtime, 'HH') FROM test");
+        r.print();
 //        CREATE TABLE hive_table (
 //                user_id STRING,
 //                order_amount DOUBLE
 //        ) PARTITIONED BY (dt STRING, hr STRING) STORED AS parquet TBLPROPERTIES (
 //                'partition.time-extractor.timestamp-pattern'='$dt $hr:00:00',
-//                'sink.partition-commit.trigger'='partition-time',
-//                'sink.partition-commit.delay'='1 h',
+//                'sink.partition-commit.delay'='1 s',
 //                'sink.partition-commit.policy.kind'='metastore,success-file'
 //);
 
+
+        // SET table.sql-dialect=default;
 //        CREATE TABLE test (
 //        rowtime TIMESTAMP(3),
-//                msg VARCHAR
+//                msg VARCHAR,
+//                WATERMARK FOR rowtime AS rowtime - INTERVAL '10' SECOND
 //) WITH (
 //                'connector' = 'kafka',
 //                'topic' = 'test',
@@ -47,16 +54,9 @@ public class StreamDataWriteToHiveTest extends FlinkJavaStreamTableTestBase {
 //                'properties.bootstrap.servers' = 'localhost:9092',
 //                'properties.group.id' = 'test',
 //                'format' = 'json'
-//        )
-// {"rowtime":"2021-01-20 11:00:23","msg":"hello"}
+//        );
         //SET table.sql-dialect=hive;
+//        INSERT INTO TABLE hive_table SELECT 'hello' as user_id, 1.1 as order_amount, DATE_FORMAT(rowtime, 'yyyy-MM-dd'), DATE_FORMAT(rowtime, 'HH') FROM test;
 
-
-        tableEnv.registerCatalog("catalogName", catalog);
-        tableEnv.useCatalog("catalogName");
-        tableEnv.getConfig().setSqlDialect(SqlDialect.HIVE);
-        TableResult r = tableEnv.executeSql("INSERT INTO TABLE hive_table SELECT msg as user_id, 1.1 as order_amount, DATE_FORMAT(rowtime, 'yyyy-MM-dd'), DATE_FORMAT(rowtime, 'HH') FROM test");
-//        INSERT INTO TABLE hive_table SELECT msg as user_id, 1.1 as order_amount, DATE_FORMAT(rowtime, 'yyyy-MM-dd'), DATE_FORMAT(rowtime, 'HH') FROM test;
-        r.print();
     }
 }
