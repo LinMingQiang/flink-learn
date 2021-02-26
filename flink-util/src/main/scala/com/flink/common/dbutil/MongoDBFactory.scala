d8a9491a0b731dad0d22311ed0c5b7933d878ac1
mqlin <1356469429@qq.com>
2021-02-27 00:39:46 +0800

    mongo factory

---
+++ flink-util/src/main/scala/com/flink/common/dbutil/MongoDBFactory.scala
@@ -0,0 +1,103 @@
+package com.flink.common.dbutil
+
+import com.mongodb.client.MongoCollection
+import com.mongodb.client.model.{UpdateOneModel, UpdateOptions}
+
+import scala.collection.JavaConverters._
+import com.mongodb.{MongoClient, MongoCredential, ServerAddress}
+import org.bson.Document
+
+import scala.collection.JavaConverters._
+
+object MongoDBFactory {
+
+  def getMongoDBConn(hostPort: String,
+                     userName: String,
+                     passw: String,
+                     databaseName: String): MongoClient = {
+    val serverAddresses = hostPort
+      .split("\\s*,\\s*")
+      .map(ele => {
+        val hostPort = ele.split(":")
+        new ServerAddress(hostPort(0), hostPort(1).toInt)
+      })
+      .toList
+    val mongoCredential = MongoCredential.createScramSha1Credential(
+      userName,
+      databaseName,
+      passw.toCharArray)
+    new MongoClient(serverAddresses.asJava, List(mongoCredential).asJava)
+  }
+
+  /**
+   *
+   * @param docs
+   */
+   def bulkWrite(collection: MongoCollection[Document], docs: Seq[Document]): Unit = {
+    val updateOptions = new UpdateOptions().upsert(true)
+    val requests =
+      docs.map { doc =>
+        val o = new Document()
+        val _id = doc.get("id")
+        val uv = doc.get("uv")
+        doc.remove("id")
+        doc.remove("uv")
+        o.append("$setOnInsert", doc)
+        o.append("$set", new Document("uv", uv))
+        new UpdateOneModel[Document](
+          new Document("_id", _id),
+          o,
+          updateOptions
+        )
+      }
+    if (requests.nonEmpty) {
+      collection.bulkWrite(requests.toList.asJava)
+    }
+  }
+
+  //
+  def main(args: Array[String]): Unit = {
+    val databaseName = "test"
+    val collectionName = "AppDayTest"
+    val connUser = "admin"
+    val connPassw = "123456"
+    val host = "localhost:27017"
+    val mongoClient = MongoDBFactory.getMongoDBConn(
+      host,
+      connUser,
+      connPassw, // 固定admin
+      "admin")
+    val db = mongoClient.getDatabase(databaseName)
+    val collection = db.getCollection(collectionName)
+    val doc = new Document()
+    doc.append("id", "id")
+    doc.append("msg", "msg")
+    doc.append("uv", 100L)
+
+    bulkWrite(collection, Seq(doc))
+
+//    val updateOptions = new UpdateOptions().upsert(true)
+//    //获取数据库连接对象
+//    //获取集合
+//    //要插入的数据
+//    val doc = new Document()
+//      .append("appkey", "test5")
+//
+//    val o = new Document()
+//    o.append("$setOnInsert", doc) // 当不存在就执行 $setOnInsert 和 $set
+//      .append("$set", new Document("uv", 2L)) // 当存在就执行$set
+//      .append("$inc", new Document("pv", 100L))
+//
+//    println(o)
+//
+//    val document = new UpdateOneModel[Document](
+//      new Document("_id", "id2"),
+//      o,
+//      updateOptions
+//    )
+//
+//    //插入一个文档
+//    import scala.collection.JavaConverters._
+//    collection.bulkWrite(List(document).asJava)
+  }
+}
--- sbin/main/sql-client.md
+++ sbin/main/sql-client.md
@@ -3,7 +3,7 @@
 正常情况下 sql-client.sh 都是在flink集群里面，但是如果我们没有flink集群只有yarn怎么办
 
 1：开启一个yarn-session
-./yarn-session.sh -tm 2192 -s 8 -jm 1024
+./bin/yarn-session.sh -tm 2192 -s 8 -jm 1024
 
 2：增加一个配置文件 vi conf/sql-client-session.yaml
 添加
