4bf11ba8df82cbef86176eb5334407163bf705f8
小弓虽 <mqlin@mob.com>
2021-04-29 21:29:28 +0800

    使用window算dau

--- flink-core/src/main/java/com/flink/learn/entry/FlinkStreamDAUEntry.java
+++ flink-core/src/main/java/com/flink/learn/entry/FlinkStreamDAUEntry.java
@@ -3,7 +3,7 @@ package com.flink.learn.entry;
 import com.core.FlinkEvnBuilder;
 import com.flink.common.core.EnvironmentalKey;
 import com.flink.common.core.FlinkLearnPropertiesUtil;
-import com.flink.common.kafka.KafkaManager;
+import com.flink.common.kafka.KafkaManager.KafkaMessge;
 import org.apache.flink.api.common.state.MapState;
 import org.apache.flink.api.common.state.MapStateDescriptor;
 import org.apache.flink.api.common.state.ValueState;
@@ -33,15 +33,15 @@ public class FlinkStreamDAUEntry {
         StreamExecutionEnvironment streamEnv = FlinkEvnBuilder.buildStreamingEnv(FlinkLearnPropertiesUtil.param(),
                 FlinkLearnPropertiesUtil.CHECKPOINT_PATH(),
                 FlinkLearnPropertiesUtil.CHECKPOINT_INTERVAL());
-        SingleOutputStreamOperator<KafkaManager.KafkaTopicOffsetTimeUidMsg> uid1T = null;
-        uid1T.keyBy((KeySelector<KafkaManager.KafkaTopicOffsetTimeUidMsg, String>) value -> value.msg()) // 按key分配
+        SingleOutputStreamOperator<KafkaMessge> uid1T = null;
+        uid1T.keyBy((KeySelector<KafkaMessge, String>) value -> value.msg()) // 按key分配
                 .window(TumblingEventTimeWindows.of(Time.seconds(100L))) // 统计100s一个窗口
                 // 因为用的是系统时间，所以一个窗口会被多次触发，除非wtm超过了这个窗口的endtime，否则窗口一直保留.用processtime可以多次触发
                 .trigger(ContinuousProcessingTimeTrigger.of(Time.seconds(3))) // 固定时间触发, 每5s触发一次(系统时间)
                 .evictor(TimeEvictor.of(Time.seconds(0), true)) // 要定时清理窗口数据，否则会一直触发，即使没有数据。trigger后触发
                 // 如果不加这个， Iterable<KafkaTopicOffsetTimeUidMsg> elements 的数据就一直累积。
                 // 每次计算完都清除 窗口数据。(只是清理原始数据process的数据会保留，也就是说，每次计算的时候，都是计算都是拿着5s中的数据进入process计算)
-                .process(new ProcessWindowFunction<KafkaManager.KafkaTopicOffsetTimeUidMsg,
+                .process(new ProcessWindowFunction<KafkaMessge,
                         Tuple2<TimeWindow, Tuple3<String, Long, Long>>,
                         String,
                         TimeWindow>() {
@@ -68,14 +68,14 @@ public class FlinkStreamDAUEntry {
                     @Override
                     public void process(String groupKey,
                                         Context context,
-                                        Iterable<KafkaManager.KafkaTopicOffsetTimeUidMsg> elements,
+                                        Iterable<KafkaMessge> elements,
                                         Collector<Tuple2<TimeWindow, Tuple3<String, Long, Long>>> out) throws Exception {
                         ValueState<Long> pvState = context.windowState().getState(pvDec);
                         ValueState<Long> uvState = context.windowState().getState(uvDec);
                         MapState<String, String> uidsState = context.windowState().getMapState(uidDec);
                         Long pv = pvState.value();
                         Long uv = uvState.value();
-                        for (KafkaManager.KafkaTopicOffsetTimeUidMsg element : elements) {
+                        for (KafkaMessge element : elements) {
                             if (!uidsState.contains(element.uid())) {
                                 uidsState.put(element.uid(), null);
                                 uv++;
