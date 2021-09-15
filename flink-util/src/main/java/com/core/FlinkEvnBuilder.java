75082545398f24aaad906f9420b789df57750d6f
小弓虽 <mqlin@mob.com>
2021-09-15 11:59:20 +0800

    state 新api

--- flink-util/src/main/java/com/core/FlinkEvnBuilder.java
+++ flink-util/src/main/java/com/core/FlinkEvnBuilder.java
@@ -2,6 +2,7 @@ package com.core;
 
 import com.flink.common.core.FlinkLearnPropertiesUtil;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.contrib.streaming.state.EmbeddedRocksDBStateBackend;
 import org.apache.flink.contrib.streaming.state.RocksDBStateBackend;
 import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -38,32 +39,26 @@ public class FlinkEvnBuilder {
             String checkpointPath,
             Long checkPointInterval) {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-//        env.setParallelism(2);
-//        env.setMaxParallelism(2);
-//        env.getConfig().setGlobalJobParameters(parameters); // 广播配置
-//        if (checkPointInterval > 0L) {
-//            env.enableCheckpointing(checkPointInterval); //更新offsets。每60s提交一次
-//            env.getCheckpointConfig().setMinPauseBetweenCheckpoints(checkPointInterval); //; 两个chk最小间隔
-//        }
-        // 默认 200
-//        env.getConfig().setAutoWatermarkInterval(20L); // 设置 触发水位计算 间隔
-//        // 同一时间只允许进行一个检查点
-//        env.getCheckpointConfig().setMaxConcurrentCheckpoints(1);
-//        // 不设置的话，任务cancle后会删除ckp
-//        env.getCheckpointConfig().enableExternalizedCheckpoints(
-//                CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
-//        //env.setStateBackend(new FsStateBackend(checkpointPath))
-//        RocksDBStateBackend rocksDBStateBackend = null;
-//        try {
-//            // enableIncrementalCheckpointing设置为ture需要注意，checkpoint可能会比较大，或者一直增大，但是savepoint却小（实际也应该小）
-//            rocksDBStateBackend = new RocksDBStateBackend(checkpointPath, true);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        // state.backend.rocksdb.ttl.compaction.filter.enabled ：这个配置现在无效了，需要在richfunc里面设置ttlconf或者sql的ttl
-//        // 说是存储在hdfs，看代码好像不支持 hdfs // rocksdb本地路径，默认在tm临时路径下
-//        // rocksDBStateBackend.setDbStoragePath(checkpointPath + "/rocksdbstorage");
-//        env.setStateBackend(rocksDBStateBackend);
+        env.getConfig().setGlobalJobParameters(parameters); // 广播配置
+        if (checkPointInterval > 0L) {
+            env.enableCheckpointing(checkPointInterval); //更新offsets。每60s提交一次
+            env.getCheckpointConfig().setMinPauseBetweenCheckpoints(checkPointInterval); //; 两个chk最小间隔
+        }
+        env.getConfig().setAutoWatermarkInterval(20L); // 设置 触发水位计算 间隔
+        // 同一时间只允许进行一个检查点
+        env.getCheckpointConfig().setMaxConcurrentCheckpoints(1);
+        // 不设置的话，任务cancle后会删除ckp
+        env.getCheckpointConfig().enableExternalizedCheckpoints(
+                CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+        // 这两个一起用
+        env.getCheckpointConfig().setCheckpointStorage(checkpointPath);
+        // enableIncrementalCheckpointing设置为ture需要注意，checkpoint可能会比较大，或者一直增大，但是savepoint却小（实际也应该小）
+        EmbeddedRocksDBStateBackend backend = new EmbeddedRocksDBStateBackend(false);
+
+        // state.backend.rocksdb.ttl.compaction.filter.enabled ：这个配置现在无效了，需要在richfunc里面设置ttlconf或者sql的ttl
+        // 说是存储在hdfs，看代码好像不支持 hdfs // rocksdb本地路径，默认在tm临时路径下
+        // rocksDBStateBackend.setDbStoragePath(checkpointPath + "/rocksdbstorage");
+        env.setStateBackend(backend);
         return env;
     }
 
