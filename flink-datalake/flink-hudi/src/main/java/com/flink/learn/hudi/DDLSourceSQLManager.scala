46ad7e0f073d88238bd402d543ac4d32897e2e1c
john.wick <john.wick@vipshop.com>
2022-01-27 20:37:24 +0800

    [FLINK HUDI] hudi建表，数据写入hudi，查询hudi。本地测试需要 编译hudi修改hadoop和hive版本为3.1.2

---
+++ flink-datalake/flink-hudi/src/main/java/com/flink/learn/hudi/DDLSourceSQLManager.scala
@@ -0,0 +1,231 @@
+package com.flink.learn.hudi
+
+import com.flink.common.core.FlinkLearnPropertiesUtil
+
+object DDLSourceSQLManager {
+
+  /**
+   * csv格式 的输入
+   *
+   * @param broker
+   * @param zk
+   * @param topic
+   * @param tableName
+   * @param delimiter
+   * @param groupID
+   * @return
+   */
+  def createStreamFromKafka_CSV(broker: String,
+                                zk: String,
+                                topic: String,
+                                tableName: String,
+                                delimiter: String,
+                                groupID: String) =
+    s"""CREATE TABLE $tableName (
+       |    topic VARCHAR,
+       |    msg VARCHAR
+       |) WITH (
+       |'connector' = 'kafka-0.10',
+       |    'topic' = '$topic',
+       |    'scan.startup.mode' = 'latest-offset',
+       |    'properties.bootstrap.servers' = '$broker',
+       |    'properties.group.id' = '$groupID',
+       |    'format' = 'csv',
+       |    'csv.field-delimiter' = '$delimiter',
+       |    'csv.ignore-parse-errors' = 'true'
+       |)""".stripMargin
+
+  // -- earliest-offset /  group-offsets
+  def createStreamFromKafkaProcessTime(broker: String,
+                                       zk: String,
+                                       topic: String,
+                                       tableName: String,
+                                       groupID: String): String = {
+    s"""CREATE TABLE $tableName (
+       |    id VARCHAR,
+       |    name VARCHAR,
+       |    age INT,
+       |    proctime as PROCTIME()
+       |) WITH (
+          'connector' = 'kafka-0.10',
+       |    'topic' = '$topic',
+       |    'scan.startup.mode' = 'latest-offset',
+       |    'properties.bootstrap.servers' = '$broker',
+       |    'properties.group.id' = '$groupID',
+       |    'format' = 'json',
+       |    'json.fail-on-missing-field' = 'false',
+       |    'json.ignore-parse-errors' = 'true'
+       |)""".stripMargin
+  }
+
+  // -- earliest-offset /  group-offsets
+  def createStreamFromKafkaEventTime(broker: String,
+                                     zk: String,
+                                     topic: String,
+                                     tableName: String,
+                                     groupID: String): String = {
+    s"""CREATE TABLE $tableName (
+       |    id VARCHAR,
+       |    name VARCHAR,
+       |    age INT,
+       |    etime TIMESTAMP(3),
+       |    WATERMARK FOR etime AS etime - INTERVAL '5' SECOND
+       |) WITH (
+          'connector' = 'kafka-0.10',
+       |    'topic' = '$topic',
+       |    'scan.startup.mode' = 'latest-offset',
+       |    'properties.bootstrap.servers' = '$broker',
+       |    'properties.group.id' = '$groupID',
+       |    'format' = 'json',
+       |    'json.fail-on-missing-field' = 'false',
+       |    'json.ignore-parse-errors' = 'true'
+       |)""".stripMargin
+  }
+
+  /**
+   *
+   * @param topic
+   * @param tableName
+   * @return
+   */
+
+  def createStreamFromKafka(topic: String,
+                            tableName: String): String = {
+    s"""CREATE TABLE $tableName (
+       |    topic VARCHAR METADATA FROM 'topic',
+       |    `offset` bigint METADATA,
+       |    msg VARCHAR
+       |) WITH (
+       |    'connector' = 'kafka',
+       |    'topic' = '$topic',
+       |    'scan.startup.mode' = 'latest-offset',
+       |    'properties.bootstrap.servers' = 'localhost:9092',
+       |    'properties.group.id' = 'test',
+       |    'format' = 'json',
+       |    'json.ignore-parse-errors' = 'true'
+       |)""".stripMargin
+  }
+
+
+  /**
+   * 窗口
+   */
+  def ddlTumbleWindow(topic: String, tableName: String): String = {
+    s"""CREATE TABLE $tableName (
+       |    username VARCHAR,
+       |    url VARCHAR,
+       |    tt bigint,
+       |    ts AS TO_TIMESTAMP(FROM_UNIXTIME(tt / 1000, 'yyyy-MM-dd HH:mm:ss')),
+       |    proctime as PROCTIME(),
+       |     WATERMARK FOR ts AS ts - INTERVAL '20' SECOND
+       |) WITH (
+          'connector' = 'kafka-0.10',
+       |    'topic' = '$topic',
+       |    'scan.startup.mode' = 'latest-offset',
+       |    'properties.bootstrap.servers' = 'localhost:9092',
+       |    'properties.group.id' = 'test',
+       |    'format' = 'json',
+       |    'json.fail-on-missing-field' = 'false',
+       |    'json.ignore-parse-errors' = 'true'
+       |)""".stripMargin
+
+  }
+
+  def createCustomSinkTbl(printlnSinkTbl: String): String = {
+    s"""CREATE TABLE ${printlnSinkTbl} (
+       |msg VARCHAR,
+       |cnt BIGINT
+       |) WITH (
+       |'connector.type' = 'printsink',
+       |'println.prefix'='>> : '
+       |)""".stripMargin
+  }
+
+  def createCustomMongoSink(tblName: String): String = {
+    s"""CREATE TABLE ${tblName} (
+       |id VARCHAR,
+       |msg VARCHAR,
+       |uv BIGINT
+       |) WITH (
+       |'connector' = 'custom-mongo',
+       |'mongourl'='localhost:27017',
+       |'mongouser'='admin',
+       |'mongopassw'='123456',
+       |'mongodb'='test',
+       |'collection'='runoob'
+       |)""".stripMargin
+  }
+
+  def createHbaseLookupSource(tblName: String): String = {
+    s"""CREATE TABLE ${tblName} (
+       |word VARCHAR
+       |) WITH (
+       |'connector' = 'custom-hbase-lookup'
+       |)""".stripMargin
+  }
+
+  /**
+   * 新版的是connector,旧版是 connector.type
+   *
+   * @param printlnSinkTbl
+   * @return
+   */
+  def createDynamicPrintlnRetractSinkTbl(printlnSinkTbl: String): String = {
+    s"""CREATE TABLE ${printlnSinkTbl} (
+       |msg VARCHAR,
+       |cnt BIGINT
+       |) WITH (
+       |'connector' = 'printRetract'
+       |)""".stripMargin
+  }
+
+
+  def createCustomPrintlnRetractSinkTbl(printlnSinkTbl: String): String = {
+    s"""CREATE TABLE ${printlnSinkTbl} (
+       |topic VARCHAR,
+       |msg VARCHAR,
+       |ll BIGINT
+       |) WITH (
+       |'connector.type' = 'printsink_retract',
+       |'println.prefix'='>> : '
+       |)""".stripMargin
+  }
+
+  def createCustomHbaseSinkTbl(hbasesinkTbl: String): String = {
+    s"""CREATE TABLE ${hbasesinkTbl} (
+       |topic VARCHAR,
+       |c BIGINT
+       |) WITH (
+       |'connector.type' = 'hbasesink'
+       |)""".stripMargin
+  }
+
+  def createFromMysql(tblName: String) =
+    s"""CREATE TABLE ${tblName} (
+       |    msg VARCHAR,
+       |    cnt BIGINT,
+       |    PRIMARY KEY (msg) NOT ENFORCED
+       |) WITH (
+       |    'connector' = 'jdbc',
+       |    'url' = '${FlinkLearnPropertiesUtil.MYSQL_HOST}',
+       |    'table-name' = '${tblName}',
+       |    'username' = '${FlinkLearnPropertiesUtil.MYSQL_USER}',
+       |    'password' = '${FlinkLearnPropertiesUtil.MYSQL_PASSW}',
+       |    'sink.buffer-flush.max-rows' = '1'
+       |)""".stripMargin
+
+  def createCustomESSink(tblName: String): String = {
+    s"""CREATE TABLE ${tblName} (
+       |id VARCHAR,
+       |msg VARCHAR,
+       |uv BIGINT
+       |) WITH (
+       |'connector' = 'custom-es',
+       |'es.address'='localhost:27017',
+       |'es.clustername'='admin',
+       |'es.passw'='123456',
+       |'es.index'='test',
+       |'es.commit.size'='runoob'
+       |)""".stripMargin
+  }
+}
--- flink-datalake/flink-hudi/src/main/java/com/flink/learn/hudi/entry.java
+++ flink-datalake/flink-hudi/src/main/java/com/flink/learn/hudi/entry.java
@@ -1,32 +1,85 @@
 package com.flink.learn.hudi;
 
+import org.apache.flink.contrib.streaming.state.EmbeddedRocksDBStateBackend;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.StatementSet;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.apache.flink.table.catalog.hive.HiveCatalog;
+import org.apache.flink.types.Row;
+import scala.Tuple2;
+
+import java.util.concurrent.ExecutionException;
 
 public class entry {
 	public static StreamTableEnvironment tableEnv = null;
-	public static String path = "hdfs://localhost:9000/tmp";
+	public static StreamExecutionEnvironment env = null;
+	public static String path = "hdfs://localhost:9000/tmp/hudi/hudi_test_msg_cnt";
+	public static String name = "hive";
+	public static String defaultDatabase = "test";
+	public static String hiveConfDir = "/Users/eminem/programe/hadoop/hive-3.1.2/conf";
 
-	public static void main(String[] args) {
+	/**
+	 * https://hudi.apache.org/cn/docs/next/flink-quick-start-guide
+	 * hudi 有个个离线 compaction
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
 		init();
-		String s = "CREATE TABLE hudi_users5(\n" +
-				"    msg STRING PRIMARY KEY NOT ENFORCED,\n" +
-				"    cnt BIGINT \n" +
-				") PARTITIONED BY (`partition`) WITH (\n" +
-				"    'connector' = 'hudi',\n" +
-				"    'table.type' = 'MERGE_ON_READ',\n" +
-				"    'path' = '" + path + "'" +
-				")";
-		tableEnv.executeSql(s);
+//		createTable("hive.test.hudi_test_msg_cnt");
+//		kafkaToHudi();
 
+		readHudi();
+	}
+
+	public static void kafkaToHudi() throws ExecutionException, InterruptedException {
+		// {"msg":"msg"}
+		// insert into hive.test.hudi_test_msg_cnt values('msg', 1);
+		String insertSql = "insert into hive.test.hudi_test_msg_cnt select msg,count(1) cnt from hive.test.src_kafka_msg group by msg";
+		tableEnv.executeSql(insertSql).await();
+	}
+
+	public static void readHudi() throws Exception {
+		tableEnv.toRetractStream(tableEnv.sqlQuery("select * from hive.test.hudi_test_msg_cnt"), Row.class)
+				.print();
+		env.execute();
 	}
 
 
+	public static void createTable(String tableName) {
+		String s = "CREATE TABLE " + tableName + "(\n" +
+				"    msg STRING PRIMARY KEY NOT ENFORCED,\n" +
+				"    cnt BIGINT \n" +
+				") PARTITIONED BY (`msg`) WITH (\n" +
+				"    'connector' = 'hudi',\n" +
+				"    'table.type' = 'MERGE_ON_READ',\n" +
+				"    'path' = '" + path + "'," +
+				"    'read.streaming.enabled' = 'true'," +
+				"    'read.streaming.start-commit' = '1643279889754'," + // read.streaming.start-commit 时间戳后的所有数据。该功能的特殊在于可以同时在流和批的 pipeline 上执行。
+				"    'read.streaming.check-interval' = '4'," + // 指定检查新的commit的周期，默认是60秒
+				"    'compaction.async.enabled' = 'false'," + // 在线compaction
+//				"    'compaction.delta_seconds' = '10'," + // 10s触发一次compaction
+				"    'index.bootstrap.enabled' = 'true'," +
+				"    'changelog.enabled' = 'true'," +
+				"    'write.tasks' = '3'" + // 并行度，其他是跟着 default并行度一起的
+
+		")";
+		System.out.println(s);
+		tableEnv.executeSql(s);
+	}
+
+	/**
+	 * 需要设置 ckp，否则数据不commit
+	 */
 	public static void init() {
-		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		env = StreamExecutionEnvironment.getExecutionEnvironment();
+		env.setParallelism(2);
+		env.getCheckpointConfig().setCheckpointInterval(10000L);
+		env.setStateBackend(new EmbeddedRocksDBStateBackend());
 		EnvironmentSettings sett = EnvironmentSettings.newInstance().useBlinkPlanner().build();
 		tableEnv = StreamTableEnvironment.create(env, sett);
+		HiveCatalog hive = new HiveCatalog(name, defaultDatabase, hiveConfDir);
+		tableEnv.registerCatalog(name, hive);
 	}
 }
