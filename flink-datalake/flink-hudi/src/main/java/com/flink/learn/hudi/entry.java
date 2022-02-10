341f4ac2be480df6c6300316464475744714fcb7
john.wick <john.wick@vipshop.com>
2022-02-10 17:59:46 +0800

    #16 hudi测试

--- flink-datalake/flink-hudi/src/main/java/com/flink/learn/hudi/entry.java
+++ flink-datalake/flink-hudi/src/main/java/com/flink/learn/hudi/entry.java
@@ -14,7 +14,7 @@ import java.util.concurrent.ExecutionException;
 public class entry {
 	public static StreamTableEnvironment tableEnv = null;
 	public static StreamExecutionEnvironment env = null;
-	public static String path = "hdfs://localhost:9000/tmp/hudi/hudi_test_msg_cnt";
+	public static String path = "hdfs://localhost:9000/tmp/hudi/";
 	public static String name = "hive";
 	public static String defaultDatabase = "test";
 	public static String hiveConfDir = "/Users/eminem/programe/hadoop/hive-3.1.2/conf";
@@ -22,49 +22,157 @@ public class entry {
 	/**
 	 * https://hudi.apache.org/cn/docs/next/flink-quick-start-guide
 	 * hudi 有个个离线 compaction
+	 *
 	 * @param args
 	 * @throws Exception
 	 */
 	public static void main(String[] args) throws Exception {
 		init();
-//		createTable("hive.test.hudi_test_msg_cnt");
-//		kafkaToHudi();
+//		createTable("hive.test.hudi_test");
+		// 在hive同步建表，这样可以通过hive查询数据
+//		createHiveSyncTable("hive.test.hudi_test");
+//		kafkaToHudi("hive.test.hudi_test");
 
-		readHudi();
+
+//		createHiveSyncCOWTable("hive.test.hudi_cow_test");
+//		kafkaToHudi("hive.test.hudi_cow_test");
+//		readHudi("hive.test.hudi_test");
 	}
 
-	public static void kafkaToHudi() throws ExecutionException, InterruptedException {
-		// {"msg":"msg"}
+	/**
+	 * insert 的顺序要和建表一样
+	 * parquet 是包含了全量数据的
+	 * @param targetTable
+	 * @throws ExecutionException
+	 * @throws InterruptedException
+	 */
+	public static void kafkaToHudi(String targetTable) throws ExecutionException, InterruptedException {
+		// 一条一个 commit。10s中一次
+		// {"msg":"1","rowtime":"2021-01-01 11:11:11"}
+		// {"msg":"2","rowtime":"2021-01-01 11:11:11"}
+		// {"msg":"3","rowtime":"2021-01-01 11:11:11"}
+		// {"msg":"4","rowtime":"2021-01-01 11:11:11"}
+		// {"msg":"5","rowtime":"2021-01-01 11:11:11"}  {"msg":"10","rowtime":"2021-01-01 11:11:11"}
+// 进行 compaction ,这时候hive查询的是 compaction 1 的文件 parquet
+		// 在输入5个
+		// {"msg":"1","rowtime":"2021-01-01 11:11:11"}
+		// {"msg":"2","rowtime":"2021-01-01 11:11:11"}
+		// {"msg":"3","rowtime":"2021-01-01 11:11:11"}
+		// {"msg":"4","rowtime":"2021-01-01 11:11:11"}
+		// {"msg":"5","rowtime":"2021-01-01 11:11:11"}  {"msg":"10","rowtime":"2021-01-01 11:11:11"}
+// 进行 compaction,这时候hive查询的是 compaction 2 的文件 parquet
+
+// 由 flink 设置 start 和 end ，可以查到每个版本的数据
+// 所以compaction的每个parquet文件里面 是包含每个commit的数据？
+
 		// insert into hive.test.hudi_test_msg_cnt values('msg', 1);
-		String insertSql = "insert into hive.test.hudi_test_msg_cnt select msg,count(1) cnt from hive.test.src_kafka_msg group by msg";
+		// hive.test.hudi_test_msg_cnt
+		String insertSql = "insert into " + targetTable + " select msg,count(1) cnt,dt from hive.test.kafka_source group by dt,msg";
 		tableEnv.executeSql(insertSql).await();
 	}
 
-	public static void readHudi() throws Exception {
-		tableEnv.toRetractStream(tableEnv.sqlQuery("select * from hive.test.hudi_test_msg_cnt"), Row.class)
+	public static void readHudi(String tbl) throws Exception {
+		tableEnv.toRetractStream(tableEnv.sqlQuery("select * from "+ tbl), Row.class)
 				.print();
 		env.execute();
 	}
 
 
+	/**
+	 * write.operator = upsert
+	 * hive可以直接查数据
+	 * 映射为 hive的两张表，一张是 _rt (增量实时表)  _ro （读优化视图）
+	 * parquet是全量数据，然后加上对应时间之后的log文件就是全量的数据
+	 * @param tableName
+	 */
+	public static void createHiveSyncTable(String tableName) {
+		String s = "CREATE TABLE " + tableName + "(\n" +
+				"    msg STRING PRIMARY KEY NOT ENFORCED,\n" +
+				"    cnt BIGINT," +
+				"	`dt` STRING\n" +
+				") PARTITIONED BY (`dt`) WITH (\n" +
+				"    'connector' = 'hudi',\n" +
+				"    'table.type' = 'MERGE_ON_READ',\n" +
+				"    'path' = '" + path + tableName + "'," +
+				// 读取的时候配置
+				"    'read.streaming.enabled' = 'true'," +
+				"    'read.streaming.start-commit' = '20220209170729182'," + // read.streaming.start-commit 时间戳后的所有数据。该功能的特殊在于可以同时在流和批的 pipeline 上执行。
+				"    'read.streaming.check-interval' = '4'," + // 指定检查新的commit的周期，默认是60秒
+				// mor的配置
+				"    'compaction.schedule.enable' = 'true'," + // 生成 compaction计划，（建议开启，可以配合离线compaction使用）
+				"    'compaction.async.enabled' = 'true'," + // 在线执行 compaction
+				"    'compaction.delta_seconds' = '10'," + // 10s触发一次compaction
+				"	 'hive_sync.enable' = 'true'," +
+				"	'hive_sync.mode' = 'hms'," +
+				"	'hive_sync.metastore.uris' = 'thrift://localhost:9083'," +
+				"	'hive_sync.db' = 'test'," +
+				"	'hive_sync.table' = 'hudi_test'," +
+				"    'index.bootstrap.enabled' = 'true'," +
+				"    'changelog.enabled' = 'false'," + // false = upsert, true = full (一条输入会产生 2条到 hudi sink)
+				"    'write.tasks' = '3'" + // 并行度，其他是跟着 default并行度一起的
+				")";
+		System.out.println(s);
+		tableEnv.executeSql(s);
+	}
+	/**
+	 * write.operator = upsert
+	 * 一个commit 一个 parquet文件，且都是全量的
+	 * @param tableName
+	 */
+	public static void createHiveSyncCOWTable(String tableName) {
+		String s = "CREATE TABLE " + tableName + "(\n" +
+				"    msg STRING PRIMARY KEY NOT ENFORCED,\n" +
+				"    cnt BIGINT," +
+				"	`dt` STRING\n" +
+				") PARTITIONED BY (`dt`) WITH (\n" +
+				"    'connector' = 'hudi',\n" +
+				"    'table.type' = 'COPY_ON_WRITE',\n" +
+				"    'path' = '" + path + tableName + "'," +
+				// 读取的时候配置
+				"    'read.streaming.enabled' = 'true'," +
+				"    'read.streaming.start-commit' = '20220209170729182'," + // read.streaming.start-commit 时间戳后的所有数据。该功能的特殊在于可以同时在流和批的 pipeline 上执行。
+				"    'read.streaming.check-interval' = '4'," + // 指定检查新的commit的周期，默认是60秒
+				"	 'hive_sync.enable' = 'true'," +
+				"	'hive_sync.mode' = 'hms'," +
+				"	'hive_sync.metastore.uris' = 'thrift://localhost:9083'," +
+				"	'hive_sync.db' = 'test'," +
+				"	'hive_sync.table' = 'hudi_test'," +
+				"    'index.bootstrap.enabled' = 'true'," +
+				"    'changelog.enabled' = 'false'," + // false = upsert, true = full (一条输入会产生 2条到 hudi sink)
+				"    'write.tasks' = '3'" + // 并行度，其他是跟着 default并行度一起的
+				")";
+		System.out.println(s);
+		tableEnv.executeSql(s);
+	}
+
+	/**
+	 * 当我们从  read.start-commit 和  read.end-commit 查询的时候:
+	 * 1: 首先输出的是，这段commit 时间段内的最新值
+	 * 2：然后再输出一条最新值，因为是流模式读取的，所以肯定会更新到最新的值
+	 * @param tableName
+	 */
 	public static void createTable(String tableName) {
 		String s = "CREATE TABLE " + tableName + "(\n" +
 				"    msg STRING PRIMARY KEY NOT ENFORCED,\n" +
-				"    cnt BIGINT \n" +
-				") PARTITIONED BY (`msg`) WITH (\n" +
+				"    cnt BIGINT," +
+				"	`dt` STRING\n" +
+				") PARTITIONED BY (`dt`) WITH (\n" +
 				"    'connector' = 'hudi',\n" +
 				"    'table.type' = 'MERGE_ON_READ',\n" +
-				"    'path' = '" + path + "'," +
+				"    'path' = '" + path + tableName + "'," +
+				// 读取的时候配置
 				"    'read.streaming.enabled' = 'true'," +
-				"    'read.streaming.start-commit' = '1643279889754'," + // read.streaming.start-commit 时间戳后的所有数据。该功能的特殊在于可以同时在流和批的 pipeline 上执行。
+				"    'read.start-commit' = '20220209170729182'," + // read.streaming.start-commit 时间戳后的所有数据。该功能的特殊在于可以同时在流和批的 pipeline 上执行。
+				"    'read.end-commit' = '20220209170729182'," + // read.streaming.start-commit 时间戳后的所有数据。该功能的特殊在于可以同时在流和批的 pipeline 上执行。
 				"    'read.streaming.check-interval' = '4'," + // 指定检查新的commit的周期，默认是60秒
-				"    'compaction.async.enabled' = 'false'," + // 在线compaction
-//				"    'compaction.delta_seconds' = '10'," + // 10s触发一次compaction
+				// mor的配置
+				"    'compaction.schedule.enable' = 'true'," + // 生成 compaction计划，（建议开启，可以配合离线compaction使用）
+				"    'compaction.async.enabled' = 'true'," + // 在线执行 compaction
+				"    'compaction.delta_seconds' = '10'," + // 10s触发一次compaction
 				"    'index.bootstrap.enabled' = 'true'," +
-				"    'changelog.enabled' = 'true'," +
+				"    'changelog.enabled' = 'false'," + // false = upsert, true = full (一条输入会产生 2条到 hudi sink)
 				"    'write.tasks' = '3'" + // 并行度，其他是跟着 default并行度一起的
-
-		")";
+				")";
 		System.out.println(s);
 		tableEnv.executeSql(s);
 	}
